894a89edab23700ef8783faa7f49f9774dde3e99
mnunez-newcom <mnunez@newcom-lcs.com>
2023-08-07 14:26:37 +0000

    [ADD] creando modulo

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Ventas Newcom',
+    'summary': """Módulo con Workflow de Ventas Mejorado""",
+    'description': """Funcionalidades
+        - Agrega etapa de validación de Orden de Venta
+        """,
+    'licence': 'OPL-1',
+    'author': 'Newcom LCS',
+    'category': 'Custom Modules/Workflows',
+    'depends': ['base', 'sale'],
+    'data': [],
+    'demo',
+    'application': True,
+}
+
+
+
