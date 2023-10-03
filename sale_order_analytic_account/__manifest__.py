2c6940f3a86fed4b799e709de0574d6ed9b2a1e6
Manuel Nuñez <mnunez@newcom-lcs.com>
2023-10-03 19:41:53 +0000

    renombrado nombre de modulo

---
+++ sale_order_analytic_account/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Sale Order Analytic Account',
+    'summary': """Fuerza creación de cuenta analítica en orden de venta""",
+    'description': """""",
+    'license': 'LGPL-3',
+    'author': 'Newcom LCS',
+    'category': 'Custom Modules/Workflows',
+    'depends': ['base', 'sale_management'],
+    'data': [ ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
+
+
+
