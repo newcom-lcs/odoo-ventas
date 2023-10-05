3c58b61cba1a655fc0c40dd16b900e5df64e4caf
Manuel Nuñez <mnunez@newcom-lcs.com>
2023-10-05 12:21:56 +0000

    agregado modulo bom

---
+++ sale_order_bom/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Sale Order B.O.M',
+    'summary': """Lista de Materiales para Ordenes de Venta""",
+    'description': """""",
+    'license': 'LGPL-3',
+    'author': 'Newcom LCS',
+    'category': 'Sales',
+    'depends': ['base', 'sale_management', 'analytic'],
+    'data': [
+        'views/sale_order_views.xml',        
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
+
+
+
