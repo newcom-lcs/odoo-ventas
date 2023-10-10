d52971cb53bdd2cf5d864f7c42f129bae23d984b
Manuel Nuñez <mnunez@newcom-lcs.com>
2023-10-10 17:10:24 +0000

    agregado display_type bom

---
+++ sale_order_bom/models/sale_order_line.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    display_type = fields.Selection(selection_add=[('line_bom', "Material"), ('line_note',)])
+    
+        # Fields specifying custom line logic
+    # display_type = fields.Selection(
+    #     selection=[
+    #         ('line_section', "Section"),
+    #         ('line_note', "Note"),
+    #     ],
+    #     default=False)
+
