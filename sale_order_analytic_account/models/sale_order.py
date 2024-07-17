40b4c432ebf37be0a5e75942d5da9990da72574d
Manuel Nuñez <89217597+mnunez-newcom@users.noreply.github.com>
2024-07-17 16:51:59 +0000

    fix: generacion de cuenta analitica

--- sale_order_analytic_account/models/sale_order.py
+++ sale_order_analytic_account/models/sale_order.py
@@ -2,20 +2,13 @@ from odoo import fields, models
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-    
-    # state = fields.Selection(selection_add=[('approved', "Aprobado"), ('sale',)])
-    
-    # def action_quotation_approve(self):
-    #     self.state = 'approved'
-    #     return
 
     def _action_confirm(self):
         result = super(SaleOrder, self)._action_confirm()
-        if result:
-            # crea una cuenta analítica si no tiene una
-            for order in self:
-                if not order.analytic_account_id:
-                    order._create_analytic_account()
+        # crea una cuenta analítica si no tiene una
+        for order in self:
+            if not order.analytic_account_id:
+                order._create_analytic_account()
                     
         return result
     
