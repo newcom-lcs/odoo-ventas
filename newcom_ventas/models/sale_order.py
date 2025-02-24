50616ff0e9e18697d0814152da67a3cc42a0b8cc
Manuel Olavarriaga <olavrriagam@gmail.com>
2025-02-24 14:35:44 -0300

    still trying to fix errors

--- newcom_ventas/models/sale_order.py
+++ newcom_ventas/models/sale_order.py
@@ -1,9 +1,9 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    opportunity_id = fields.Many2one('crm.lead', string='Opportunity', store=True)
+    opportunity_id = fields.Many2one('crm.lead', string='Opportunity', domain=[('type', '=', 'opportunity')], store=True)
     business_unit_id = fields.Many2one('business.unit', string='Business Unit', related='opportunity_id.business_unit_id', store=True)
     margen_teorico = fields.Float(string="Margen Teorico (%)", help="Percentage margin to add to the sales order", store=True)
     mes_cierre_facturacion = fields.Date(string="Mes de Cierre (Facturación)", help="Date to generate the invoice", store=True)
@@ -65,3 +65,31 @@ class SaleOrder(models.Model):
             if order.state == 'sent':
                 order.write({'state': 'approved'})
         return True
+
+class SaleReport(models.Model):
+    _inherit = "sale.report"
+
+    tipo_negocio = fields.Selection([
+        ('mantenimiento', 'Mantenimiento'),
+        ('cajas', 'Cajas'),
+        ('proyectos', 'Proyectos')],
+        string="Tipo de Negocio", readonly=True)
+    margen_teorico = fields.Float(string="Margen Teorico (%)", readonly=True)
+    business_unit_id = fields.Many2one('business.unit', string='Business Unit', readonly=True)
+    mes_cierre_facturacion = fields.Date(string="Mes de Cierre (Facturación)", readonly=True)
+
+    def _select(self):
+        return super(SaleReport, self)._select() + """
+            , s.tipo_negocio
+            , s.margen_teorico
+            , s.business_unit_id
+            , s.mes_cierre_facturacion
+        """
+    
+    def _group_by(self):
+        return super(SaleReport, self)._group_by() + """
+            , s.tipo_negocio
+            , s.margen_teorico
+            , s.business_unit_id
+            , s.mes_cierre_facturacion
+        """
