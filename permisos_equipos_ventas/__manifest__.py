a8157c0881002338c7901826d013220273c80a1d
Manuel Olavarriaga <olavrriagam@gmail.com>
2025-03-20 15:52:05 -0300

    Enhance sales team access by adding related purchase orders and deliveries visibility, updating security rules, and ensuring chatter access in purchase orders and stock pickings.

--- permisos_equipos_ventas/__manifest__.py
+++ permisos_equipos_ventas/__manifest__.py
@@ -3,24 +3,28 @@
     "version": "1.0",
     'license': 'LGPL-3',
     'author': 'Newcom LCS',
-    "depends": ["sale_management", "sales_team", "crm", "stock", "purchase", "mail"],
+    "depends": ["sale_management", "sales_team", "crm", "stock", "purchase", "mail", "sale_purchase", "sale_stock"],
     "summary": "Manage sales orders visibility by team and read-only access to inventory and purchases.",
     "description": """
         - Restrict sales orders visibility based on team membership.
         - Make team_id field read-only for sales orders.
         - Provide read-only access to inventory and purchase data for sales users.
         - Allow sales users to interact with chatter on all documents.
+        - Allow sales team members to see related purchase orders and deliveries in their sales orders.
     """,
     "category": "Sales",
     "data": [
         "security/sales_team_security.xml",
+        "security/sales_team_access.xml",
         "security/sales_readonly_rules.xml",
         "security/ir.model.access.csv",
         "views/sale_order_view.xml",
         'views/sale_order_menu.xml',
         'views/res_company.xml',
         'views/sales_readonly_views.xml',
-        'views/sale_order_smartbutton_view.xml'
+        'views/sale_order_smartbutton_view.xml',
+        'views/purchase_order_view.xml',
+        'views/stock_picking_view.xml'
     ],
     "application": False,
     "installable": True,
