09e69edf4232424209c4de7dd69fa2a839bf0950
Manuel Olavarriaga <olavrriagam@gmail.com>
2025-03-21 11:13:47 -0300

    Remove business unit validation and readonly logic from CRM lead model

--- newcom_ventas/models/crm_lead.py
+++ newcom_ventas/models/crm_lead.py
@@ -1,5 +1,4 @@
 from odoo import models, fields, api
-from odoo.exceptions import ValidationError
 
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
@@ -8,25 +7,4 @@ class CrmLead(models.Model):
         'business.unit',
         string='Business Unit',
         store=True
-    )
-
-    @api.constrains('business_unit_id', 'probability')
-    def _check_business_unit_change(self):
-        for record in self:
-            if record.probability in [0, 100] and 'business_unit_id' in record._get_modified_fields():
-                raise ValidationError('Business Unit cannot be modified when opportunity is won or lost.')
-
-    @api.depends('probability')
-    def _compute_business_unit_readonly(self):
-        for record in self:
-            record.business_unit_readonly = record.probability in [0, 100]
-
-    business_unit_readonly = fields.Boolean(
-        compute='_compute_business_unit_readonly',
-        store=True
-    )
-
-    @api.onchange('business_unit_readonly')
-    def _onchange_business_unit_readonly(self):
-        if self.business_unit_readonly:
-            self.business_unit_id.readonly = True 
+    ) 
